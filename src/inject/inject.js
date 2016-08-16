b857a7bb7d0c683fa116454a324eb69b9a86f6d6
Dylan <ddmills@gmail.com>
2016-08-16 11:18:27 -0600

    Add files via upload

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,42 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+			function clean()
+			  {
+					var feed = $(".feed")[0];
+					if (feed)
+					{
+						var activities = $(".activity");
+						var elementsToRemove = [];
+						for (i = 0; i < activities.length; i++)
+						{
+							if ((commute = activities[i].getElementsByClassName("workout-type")[0]) && commute.innerHTML == "Commute")
+							{
+								elementsToRemove.push(activities[i]);
+								elementsToRemove.push(activities[i].nextElementSibling);
+							}
+						}
+						for (i = 0; i < elementsToRemove.length; i++)
+						{
+							feed.removeChild(elementsToRemove[i]);
+						}
+						var timeHeaders = [].slice.call($(".time-header"));
+						for (i = 0; i < timeHeaders.length; i++)
+						{
+							if (!timeHeaders[i].nextElementSibling || timeHeaders[i].nextElementSibling.className == "time-header")
+							{
+								feed.removeChild(timeHeaders[i]);
+							}
+						}
+					}
+			  }
 
-	}
+			$(document).ready(function() {
+				clean();
+				$(".load-feed").change(clean);
+			});
+		}
 	}, 10);
 });
